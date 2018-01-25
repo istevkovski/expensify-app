5ac60cda420c705e50cc018249a75b995263198f
Ivan Stevkovski <istevkovski@outlook.com>
2018-01-25 19:53:36 +0100

    Improved login page style

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ export const LoginPage = ({ startLogin }) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">Expensify</h1>
       <p>It's time to get your expenses under control.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      <button className="button button--login" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
 );
